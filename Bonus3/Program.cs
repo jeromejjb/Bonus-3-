30ce3bbd4fbdd4f27f37acd4211dc5bdac25aa2f
jerome <jeromebrown97@gmail.com>
2021-06-04 23:09:29 -0500

    correct my bonus 3 lab

--- Bonus3/Program.cs
+++ Bonus3/Program.cs
@@ -6,88 +6,45 @@ namespace Bonus3
     {
         static void Main(string[] args)
         {
-
-
-            Random r = new Random();
-            int number = r.Next(1, 101);
-            int tries = 0;
-
-            
+            string userAnswer = "y";
 
             Console.WriteLine("Welcome to the Guess a Number Game!");
             Console.WriteLine("++++++++++++++++++++++++++++++");
-
-            Console.WriteLine("I am thinking of a number between 1 and 100.");
-            Console.WriteLine("Try to guess it, n00b.");
-
-
-            string response = "";
-            while (response != "Match!")
+            while (userAnswer == "y")
             {
-                int num = GetUserGuess();
+                Console.WriteLine("I am thinking of a number between 1 and 100.");
+                Console.WriteLine("Try to guess it, n00b.");
+                int tries = 0;
+                Random r = new Random();
+                int randomNumber = r.Next(1, 101);
+                
+                
 
-                response = Guess(num, number);
-                Console.WriteLine(response);
-                Console.WriteLine();
+                int numberOfGuesses = 1;
+                string response = "";
+
+                while (response != "Match!")
+                {
+                    Console.WriteLine("Enter your lousy guess");
+                    int userGuess = int.Parse(Console.ReadLine());
+                    response = CheckGuess(userGuess, randomNumber);
+                    Console.WriteLine(response);
+
+                    numberOfGuesses++;
+
+                }
+                Console.WriteLine($"it took you {numberOfGuesses} to guess {randomNumber}");
+                Console.WriteLine("Try again? (y/n)");
+                userAnswer = Console.ReadLine();
+
+
             }
 
-            Console.WriteLine($"it took you {tries} to guess {number}");
 
-            Console.WriteLine();
-            Console.WriteLine("This version guesses starting at one and ticks up to 100");
-            int guess = 1;
-            response = "";
-            while (response != "Match!")
-            {
-                response = GetUserGuess(, number);
-                if (response != "Match!")
-                {
-                    guess++;
-
-
-                }
-
-            }
         }
 
-        private static int GetUserGuess()
-        {
-            throw new NotImplementedException();
-        }
 
-        public static int GetUserGuess(int current, int number)
-        {
-            while (true)
-            {
-                Console.WriteLine("Please guess a number between 1 and 100 and I will tell how close you are");
-                try
-                {
-                    int num = int.Parse(Console.ReadLine());
-                    if (num < 1)
-                    {
-                        throw new Exception("That number is too small, please input a number between 1 and 100");
-                    }
-                    else if (num > 100)
-                    {
-                        throw new Exception("That number is too large, please inptu a number between 1 and 100");
-                    }
-                    return num;
-
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("That was not a valid number please try again");
-                    continue;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    continue;
-                }
-            }
-        }
-
-        public static string Guess(int guess, int number)
+        public static string CheckGuess(int guess, int number)
         {
             if (guess == number)
             {
